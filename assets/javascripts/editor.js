7c2854a678a0f59c0050812c3c1d966c9f14b3fb
Adam Wells <adamwells.dev@gmail.com>
2015-05-28 19:29:20 -0700

    begin working on file export

--- assets/javascripts/editor.js
+++ assets/javascripts/editor.js
@@ -1,4 +1,14 @@
-var editor = $('#input-box');
-editor.bind('input', function () {
-	$('.preview').html(markdown.toHTML(editor.val()));
+var input = $('.input-box');
+input.bind('input', function () {
+	$('.rendered-markdown').html(markdown.toHTML(input.val()));
 })
+
+var downloadButton = $('.download');
+downloadButton.bind('click', function () {
+	getMardownFile(input);
+})
+
+var getMardownFile = function(input, filename) {
+	var content = input.val();
+	console.log(content);
+}
